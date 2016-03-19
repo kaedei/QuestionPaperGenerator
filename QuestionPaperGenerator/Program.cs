2f779db6cd70ae154d08835ca5c0806dc7a2fee2
kaedei <kaedei@live.cn>
2016-03-19 11:40:17 +0800

    refactor

--- QuestionPaperGenerator/Program.cs
+++ QuestionPaperGenerator/Program.cs
@@ -15,43 +15,26 @@ namespace QuestionPaperGenerator
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Opening workbook...");
+			var questions = ParseQuestionItems();
+			Console.WriteLine("Generating papers...");
+			GenerateRandomPapers(questions);
+		}
 
-			var workbook = new XSSFWorkbook(File.OpenRead("question.xlsx"));
-			var sheet = workbook.GetSheet("Sheet1");
-
-			//read question from sheet1
-			var questions = new List<QuestionItem>();
-			for (int i = 0; i < sheet.LastRowNum + 1; i++)
-			{
-				var row = sheet.GetRow(i);
-				var questionItem = new QuestionItem
-				{
-					Answer = row.Cells[0].ToString(),
-					Id = row.Cells[1].ToString(),
-					Question = row.Cells[2].ToString(),
-					Options = new List<string>(6)
-				};
-				for (int columnIndex = 3; columnIndex < row.Cells.Count; columnIndex++)
-				{
-					questionItem.Options.Add(row.Cells[columnIndex].ToString());
-				}
-				questions.Add(questionItem);
-			}
-
-			//generate papers and answers
-			for (int i = 0; i < 60; i++)
+		private static void GenerateRandomPapers(List<QuestionItem> questions, int paperCount = 60, string outputDirectory = "Papers")
+		{
+			for (int i = 0; i < paperCount; i++)
 			{
 				var exportWorkbook = new XSSFWorkbook();
 				var sheet1 = exportWorkbook.CreateSheet(i.ToString());
 				//create header
 				var headerRow = sheet1.CreateRow(0);
-				headerRow.CreateCell(0).SetCellValue("填写答案");
+				headerRow.CreateCell(0).SetCellValue("答案（请填写A、B、C、D）");
 				headerRow.CreateCell(1).SetCellValue("编号");
 				headerRow.CreateCell(2).SetCellValue("问题");
 				int maxOptionCount = questions.Max(q => q.Options.Count);
 				for (int optionIndex = 0; optionIndex < maxOptionCount; optionIndex++)
 				{
-					headerRow.CreateCell(optionIndex + 3).SetCellValue("选项" + (char) ('A' + optionIndex));
+					headerRow.CreateCell(optionIndex + 3).SetCellValue("选项" + (char)('A' + optionIndex));
 				}
 
 				var rnd = new Random();
@@ -71,14 +54,40 @@ namespace QuestionPaperGenerator
 				sheet1.SetColumnHidden(1, true);
 				sheet1.CreateFreezePane(1, 1);
 				exportWorkbook.SetActiveSheet(0);
-				
-				Directory.CreateDirectory("Papers");
-				var filePath = @"Papers\" + i + ".xlsx";
+
+				Directory.CreateDirectory(outputDirectory);
+				var filePath = Path.Combine(outputDirectory, i + ".xlsx");
 				using (var fs = new FileStream(filePath, FileMode.Create))
 				{
 					exportWorkbook.Write(fs);
 				}
 			}
+		}
+
+		private static List<QuestionItem> ParseQuestionItems(string xlsxFilePath = "question.xlsx", string sheetName = "Sheet1")
+		{
+			var workbook = new XSSFWorkbook(File.OpenRead(xlsxFilePath));
+			var sheet = workbook.GetSheet(sheetName);
+
+			//read question from sheet1
+			var questions = new List<QuestionItem>();
+			for (int i = 0; i < sheet.LastRowNum + 1; i++)
+			{
+				var row = sheet.GetRow(i);
+				var questionItem = new QuestionItem
+				{
+					Answer = row.Cells[0].ToString(),
+					Id = row.Cells[1].ToString(),
+					Question = row.Cells[2].ToString(),
+					Options = new List<string>(6)
+				};
+				for (int columnIndex = 3; columnIndex < row.Cells.Count; columnIndex++)
+				{
+					questionItem.Options.Add(row.Cells[columnIndex].ToString());
+				}
+				questions.Add(questionItem);
+			}
+			return questions;
 		}
 	}
 }
